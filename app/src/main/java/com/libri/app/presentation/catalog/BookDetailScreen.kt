eb4d836c159c4696a3920aab044d5fdcca3d7a98
Murat Zalov <mzallov@yandex.ru>
2026-06-01 16:37:39 +0300

    fix: remove duplicate ExperimentalMaterial3Api import, add mutableStateOf/setValue imports

--- app/src/main/java/com/libri/app/presentation/catalog/BookDetailScreen.kt
+++ app/src/main/java/com/libri/app/presentation/catalog/BookDetailScreen.kt
@@ -24,7 +24,6 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Divider
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
@@ -40,7 +39,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
@@ -70,7 +71,7 @@ fun BookDetailScreen(
 ) {
     val uiState by viewModel.uiState.collectAsState()
     val snackbarHostState = remember { SnackbarHostState() }
-    var showFragment by androidx.compose.runtime.remember { androidx.compose.runtime.mutableStateOf(false) }
+    var showFragment by remember { mutableStateOf(false) }
     val bottomSheetState = rememberModalBottomSheetState(skipPartiallyExpanded = true)
 
     LaunchedEffect(bookId) { viewModel.loadBook(bookId, userId) }
